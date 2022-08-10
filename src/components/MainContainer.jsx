f6f8047d76f49613bb398332a30d6432ee043267
sazzy-josh <idahosajoshua61@gmail.com>
2022-08-10 17:24:37 +0100

    check fix

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -25,7 +25,7 @@ const MainContainer = () => {
 
       <motion.main
        initial={{opacity:0 ,x:-450}}
-       animate={{opacity:1 , x: 10}}
+       animate={{opacity:1 , x: 0}}
        transition={{type:'spring' , stiffness:400 , delay: 0.5  }}
       className='grid md:grid-cols-2 p-5 sm:p-10 lg:px-32 gap-10 h-auto screen box-border'>
       {/* Column 1 for medium and  large screens */}
