0b8f3a5854ad6b592a44d509907fc9a4def5aaf9
sazzy-josh <idahosajoshua61@gmail.com>
2022-08-12 20:40:20 +0100

    check fix

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -75,9 +75,9 @@ const MainContainer = () => {
         {/* Start of Main Page section */}
 
       <motion.main
-       initial={{opacity:0 ,x:-450}}
+       initial={{opacity:0 ,x:"-100vw"}}
        animate={{opacity:1 , x: 0}}
-       transition={{type:'spring' , stiffness:300 , delay: 0.7 , duration: 0.8  }}
+       transition={{type:'spring' , stiffness:200 , delay: 1 , duration: 1  }}
       className='grid md:grid-cols-2 p-5 sm:p-10 lg:px-32 gap-10 h-auto overflow-hidden screen'>
       {/* Column 1 for medium and  large screens */}
      <div className='sm:h-full relative  flex justify-center flex-col pt-4 '>
