ef881493d42772b78aa72d9cae933c8fba27f465
sazzy-josh <idahosajoshua61@gmail.com>
2022-08-23 03:29:43 +0100

    update:checkout  page successfully updated

--- src/components/checkoutSuccessful.jsx
+++ src/components/checkoutSuccessful.jsx
@@ -4,7 +4,7 @@ import Lottie from 'react-lottie';
 import success from "./assets/json/success.json"
 import confettii from "./assets/json/confetti.json"
 import { Link } from 'react-router-dom'
-
+import { HiOutlineArrowNarrowLeft } from 'react-icons/hi'
 
 const CheckoutSuccessful = () => {
     const { tx } = useParams()
@@ -13,38 +13,48 @@ const CheckoutSuccessful = () => {
         loop: 1,
         autoplay: true, 
         animationData: success,
+      
         rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
+          preserveAspectRatio: 'xMidYMid slice',
+          isClickToPauseDisabled:true,
         }
       };
       const confetti = {
-        loop: 1,
-        autoplay: true, 
+        loop: 2,
+        autoplay: 1, 
         animationData: confettii,
+      
         rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
+          preserveAspectRatio: 'xMidYMid slice',
+          isClickToPauseDisabled:true,
         }
       };
 
 
   return (
-    <div className='success flex justify-center items-center flex-col'>
-        <div className='w-[70vw] flex justify-center items-center relative '>
-        <Lottie options={defaultOptions}
-              height={400}
-              width={400}
-             />
-        <div className='absolute'>
-        <Lottie options={confetti}
-              height={1400}
-              width={800}
+    <div className='flex flex-col'>
+    
+
+        <div className='success flex justify-center items-center flex-col'>
               
-             />
-        </div>
-        </div>
-        <p className='font-semibold py-1'>Thank you {tx}!Your Payment has been recieved and your order would be delivered Shortly.</p>
-        <p className='font-semibold'>Transaction reference: FX0{Date.now()} </p>
-       <Link to='/'> <span className='rounded-full p-2 border-red-300 text-center font-semibold cursor-pointer'>Back to home</span></Link>
+           <div className='w-[70vw] flex justify-center items-center relative '>
+           <Lottie options={defaultOptions}
+                 height={400}
+                 width={400}
+                />
+           <div className='absolute'>
+           <Lottie options={confetti}
+                 height={500}
+                 width={400}
+                 
+                />
+           </div>
+           </div>
+           <p className='font-semibold py-1'>Thank you {tx}!Your Payment has been recieved and your order would be delivered Shortly.</p>
+           <p className='font-semibold'>Transaction reference: FX0CC{Date.now()} </p>
+           <Link to='/'><div className='flex my-4 gap-x-1 border-rounded justify-center border-slate-200 items-center text-black  border-2 font-semibold hover:bg-slate-700 hover:text-white rounded-full p-2'> <p> Back to home</p> <HiOutlineArrowNarrowLeft /> </div></Link>
+       </div>
+       
     </div>
   )
 }
