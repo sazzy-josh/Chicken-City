72ebcc121158eeb5a1247e7af47aa68bfc89ce53
sazzy-josh <idahosajoshua61@gmail.com>
2022-09-02 14:52:54 +0100

    update:product details page

--- src/pages/foodDetails.jsx
+++ src/pages/foodDetails.jsx
@@ -3,8 +3,8 @@ import { motion } from 'framer-motion'
 import CommonSection from '../components/commonSection'
 import { useParams } from 'react-router-dom'
 import products from '../components/assets/fake-data/products'
-
-
+import { IoMdStarOutline , IoMdStar } from 'react-icons/io'
+import { HiPlus , HiMinus  } from 'react-icons/hi'
 
 const FoodDetails = () => {
   const { id }= useParams()
@@ -13,21 +13,21 @@ const FoodDetails = () => {
     return item.id === id    
   })
 
-   const { image01 , image02, image03, quantity , category , rating , price , title } = foodDetails
-
+   const { image01 , image02, image03, quantity , category , rating , price , title , desc } = foodDetails
    
+   const [ showSize  , setSize] = useState('small')
+   const [ showDescription  , setShowDescription] = useState('desc')
     const [ showcaseImage  , setShowcaseImage] = useState(image01)
     const [ activeImage  , setActiveImage] = useState(1)
     
-    // useEffect(() => {
-      
-    //   const setImage = () => {
-    //     setShowcaseImage()
-    //   }
-    //   return () => {
-        
-    //   };
-    // }, [showcaseImage]);
+
+   
+
+    useEffect(() => {  
+     
+    
+
+    }, [foodDetails]);
     
 
     console.log(foodDetails)
@@ -35,7 +35,7 @@ const FoodDetails = () => {
   return (
     <motion.div>
    <CommonSection  title={title}></CommonSection>
-      <div className='grid md:grid-cols-2 p-5 sm:p-10 lg:px-28 h-[60vh] w-full'>
+      <div className='grid md:grid-cols-2 p-5 sm:p-10 lg:px-28 sm:h-[60vh] w-full'>
         {/* Image Section */}
       <div className='w-full flex justify-between mb-4'>
         <div className='w-1/6'>
@@ -59,8 +59,55 @@ const FoodDetails = () => {
       </div>
 
       {/* Products Details section */}
-      <div>2</div>
+      <div className='flex flex-col gap-y-1'>
+        <h3 className='md:text-xl text-md font-bold'>{title}</h3>
+        {/* <h3 className='md:text-xl text-sm font-semibold text-slate-500'>Category:{category}</h3> */}
+        <span className='flex gap-x-1'>{<> 
+         <strike className='md:text-lg text-sm font-semibold text-orange-700'>₦{<>{price + 1000}</>}</strike> <p className='md:text-lg text-sm font-semibold'>₦{price.toLocaleString()}</p> 
+        </>}</span>
+        <p className='flex gap-x-1 items-center'>{ [...Array(5)].map((_ , i) => (
+           <span className='flex items-center'> {rating > i ? <IoMdStar className=' text-yellow-400' /> : <IoMdStarOutline className=' text-slate-700' />  }  </span> 
+         ))
+         }<span className='text-slate-500 text-xs flex items-center'> {Math.ceil(Math.random()*50)} (users rating)</span> </p>
 
+         <div className='flex flex-col items-center gap-x-2 text-sm my-2 underline-offset-2'>
+         <div className='flex w-full gap-x-2 md:text-md text-sm'>
+         <p className={`cursor-pointer ${showDescription === 'desc' ? "font-semibold text-orange-700 underline" : "" }`} onClick={() =>setShowDescription("desc")}>Description</p>  <p className={`cursor-pointer  ${showDescription === 'reviews' ? "font-semibold text-orange-700 underline" : "" } `} onClick={() =>setShowDescription("reviews")} >Reviews</p>
+         </div>
+
+          <>{ 
+         showDescription === "desc" ? (<div className='flex flex-col gap-x-1 py-1 mb-1'>
+           <p className='text-[14px] tracking-wide'>{desc}</p>
+          <div className='sm:flex justify-between'>
+         <div>  
+        <p className='font-semibold my-1  text-md'>Select size</p> 
+
+        <div className='flex gap-y-1 sm:gap-y-0  gap-x-1 text-xs items-center'> 
+
+        <span onClick={() => setSize("small")} className={`rounded-lg p-2 border border-slate-300 shadow-md cursor-pointer  ${showSize === 'small' ? "border-slate-600 border-2" : "" }`}>Small</span>
+        <span  onClick={() => setSize("medium")} className={`rounded-lg p-2 border border-slate-300 shadow-md cursor-pointer ${showSize === 'medium' ? "border-slate-600 border-2" : "" } `}>Medium</span>
+        <span   onClick={() => setSize("large")} className={`rounded-lg p-2  border border-slate-300 shadow-md cursor-pointer ${showSize === 'large' ? "border-slate-600 border-2" : "" } `}>Large</span>
+
+         </div>
+        </div>
+         <div className='flex flex-col sm:justify-center sm:px-8'> <span className='font-semibold text-md my-1 w-full flex'>Quantity</span> <span className='flex gap-y-1 sm:gap-y-0 gap-x-1 items-center'>
+          <span className='font-semibold flex items-center gap-x-2'>
+           <span className='border rounded-full border-slate-300 p-2'> <HiMinus /> </span>
+           <span>{quantity}</span>
+           <span className='border rounded-full border-slate-300 p-2'> <HiPlus />  </span>
+          </span>
+          </span>
+           </div>
+          </div>
+        </div>) :
+         (<div className='w-full flex'>
+          Reviews
+          </div>)
+        } </>
+         </div>
+         
+      </div>
+      
       </div>
 
 
