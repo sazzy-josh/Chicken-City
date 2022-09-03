8e6689c655993eb752a5f7f1e8a38bfd93385bea
sazzy-josh <idahosajoshua61@gmail.com>
2022-09-03 23:03:44 +0100

    update:product details page,related products added

--- src/pages/foodDetails.jsx
+++ src/pages/foodDetails.jsx
@@ -1,18 +1,18 @@
 import { motion } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 import { IoMdStar, IoMdStarOutline } from 'react-icons/io'
-import { useParams , useNavigate } from 'react-router-dom'
+import { useParams , useNavigate ,Link } from 'react-router-dom'
 import products from '../components/assets/fake-data/products'
 import CommonSection from '../components/commonSection'
 import { useCartsContext } from '../context/CartContext'
 import { toast } from 'react-toastify';
 import { useAuthsContext } from '../context/authContext'
-
+import { TiDelete} from 'react-icons/ti'
 
 
 const FoodDetails = () => {
   const { User , loginState } = useAuthsContext() 
-  const {  singleCheckout ,  addToCart  , state:{ cartItems }  } = useCartsContext() 
+  const {  singleCheckout ,  addToCart  , state:{ cartItems } , removeFromCart  } = useCartsContext() 
 
   const navigate = useNavigate()
   const { id }= useParams()
@@ -46,6 +46,19 @@ const FoodDetails = () => {
     const [ showcaseImage  , setShowcaseImage] = useState(image01)
     const [ activeImage  , setActiveImage] = useState(1)
     
+    const addItem = () => {
+      addToCart({ id , price , title ,image01 , quantity , category})
+      // toast.success("Item Added to Cart!")
+      toast.success(`${title} added to cart!`, {
+         position: toast.POSITION.TOP_RIGHT, 
+    });
+   }
+
+   const remove = () => {
+      toast.info("Item Removed from cart!" , { position: toast.POSITION.TOP_LEFT }) 
+       return   removeFromCart({id})
+   }
+
  
     const addNewItem = () => {
       const existingItem = cartItems.find((item) => item.id === id )
@@ -159,6 +172,43 @@ const FoodDetails = () => {
       </div>
 
 
+    {/* You might also like Section */}
+    <div className='w-full flex flex-col   p-5 sm:p-10 lg:px-28'>
+      <h3 className='font-semibold text-md my-4'>You might also like</h3>
+     <div className='flex gap-x-1 gap-y-4 sm:gap-x-6 flex-wrap'> {
+    relatedProducts.map(({ id, price ,image01 , title , quantity , category})=> {
+      return (
+        <div className='flex items-center justify-center' key={id}>
+                  
+        <motion.div
+         initial={{opacity:0}}
+         animate={{opacity:1}}
+         exit={{opacity:1}}
+         transition={{duration:0.5 }}
+         key={id}
+         className="rounded-lg flex border border-red-200 flex-col w-[180px] h-[250px]  justify-around items-center relative hover:bg-gradient-to-r  hover:from-slate-50 hover:to-red-200 hover:border-red-400 hover:border-dotted shadow-xl cursor-pointer">
+      <span class="animate-ping absolute  h-4 w-4 top-0 right-0  z-10 rounded-full bg-red-400 opacity-75"></span>
+           <motion.img 
+           whileHover={{scale:1.2}}
+           src={image01} alt={title} className="sm:w-[90px] sm:h-[90px] w-[80px] h-[80px] object-contain py-1 "/>
+           <Link to={`/food-details/${id}`}>
+           <span className='lg:text-sm text-xs text-center py-2 font-bold hover:underline'>{title}</span>
+           </Link>
+           <div className='flex text-sm text-center py-2 font-bold justify-between items-center w-full p-2 '><p className=' p-1 rounded-lg text-slate-800'>₦{price.toLocaleString()}</p> {cartItems && !cartItems.some((item) => item.id === id ) ? (<span onClick={() => {
+              addItem({ id , price , title ,image01 , quantity ,category})
+           }} className='text-lg px-2 rounded text-white bg-red-300  hover:bg-white hover:text-red-400' 
+           >+</span>)  :(<span disabled onClick={remove} className='text-lg px-2 rounded  text-white bg-red-300  hover:bg-white hover:text-red-400' 
+           ><TiDelete className='w-6 h-6' /></span>)} 
+           </div>
+        <div className='absolute top-0 text-slate-900 right-0 bg-gradient-to-br from-slate-50 to-red-300 font-semibold rounded-tr-lg rounded-bl-lg text-xs overflow-hidden p-1'>10% OFF</div>
+       </motion.div>
+      </div>
+      )
+    }).slice(0,4)
+
+     } </div>
+
+    </div>
     </motion.div>
   )
 }
